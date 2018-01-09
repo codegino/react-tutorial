2ef43f95c702be1aa742b1c7f84165e70e01d93b
gihooh <carloginocatapang@gmail.com>
2018-01-09 09:14:43 +0800

    Section 4 Assignment.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Validator from './component/section4/Validator'
 import './App.css';
-import UserInput from './component/section3/UserInput'
-import UserOutput from './component/section3/UserOutput'
 
 class App extends Component {
   state = {
-    userName: 'carlo'
+    length: 0
   }
-
-  changeUsernameHandler = (event) => {
-      this.setState({
-          userName: event.target.value
-      })
+  changeTextHandler = (event) => {
+    const length = event.target.value.length;
+    this.setState({
+      length
+    })
   }
 
   render() {
     return (
       <div className="App">
-        <UserInput changed={this.changeUsernameHandler} value={this.state.userName}/>
-        <UserOutput userName={this.state.userName}/>
-        <UserOutput userName={this.state.userName}/>
+        <input type="text" onChange={this.changeTextHandler}/>
+        <Validator length={this.state.length}/>
       </div>
     );
   }
---
+++ src/component/section4/Validator.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const validator = (props) => {
+  return <h1>Number of characters is {props.length}</h1>
+}
+
+export default validator
